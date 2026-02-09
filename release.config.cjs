29e2f17b97beabfc8d1f03def0ad281e4ee625c5
Ryan Alyn Porter <rap@endymion.com>
2026-02-08 22:40:00 -0500

    ci: stabilize release notes generation

--- release.config.cjs
+++ release.config.cjs
@@ -2,7 +2,15 @@ module.exports = {
   branches: ['main'],
   plugins: [
     ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
-    ['@semantic-release/release-notes-generator', { preset: 'conventionalcommits' }],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        writerOpts: {
+          committerDate: () => ''
+        }
+      }
+    ],
     ['@semantic-release/npm', { pkgRoot: 'packages/springstack', npmPublish: false }],
     ['@semantic-release/exec', { publishCmd: 'cd packages/springstack && npm publish --access public --provenance' }],
     '@semantic-release/github'
